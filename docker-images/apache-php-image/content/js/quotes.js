fc0191ef7da0f221dbd9fe31c40e88e735351067
AkessonHenrik <henrik.akesson@heig-vd.ch>
2016-06-08 15:09:23 +0200

    Modified js, html and css

--- docker-images/apache-php-image/content/js/quotes.js
+++ docker-images/apache-php-image/content/js/quotes.js
@@ -1,15 +1,14 @@
 $(function() {
   console.log("loading quotes");
   function loadQuotes() {
-        $.getJSON("/api/quotes/", function(quotes) {
-                console.log(quotes);
-                var message = "Nothing is here";
-                if(quotes.length > 0) {
-                        message = quotes[0].name + " - " + quotes[0].quote;
-                        console.log(message);
-                }
-                $(".network-name").text(message);
-        });
+    $.getJSON("/api/quotes/", function(quotes) {
+      var message = "hello"                
+        if(quotes.length > 0) {
+          message = "<strong>" + quotes[0].name + "</strong><br/><br/>" + quotes[0].quote;
+          console.log(message);
+        }
+      $(".quotes").html(message);
+    });
   }
   loadQuotes();
   setInterval(loadQuotes, 2000);
